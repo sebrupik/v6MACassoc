3b24fa72951480291046b24e747c24c8edf62290
Seb Rupik <sebrupik@gmail.com>
2015-03-23 17:02:09 +0000

    everything

--- v6MACassoc/src/v6macassoc/objects/ipv6neighFactory.java
+++ v6MACassoc/src/v6macassoc/objects/ipv6neighFactory.java
@@ -27,11 +27,49 @@ class ipv6neighFactory {
     }
     
     private static String ipv6FullLength(String oriIPv6) {
-        String[] elements = oriIPv6.split(":");
-        byte[] byte_order = new byte[elements.length];
-        
-        for(int i=0;i<elements.length; i++) {
-            byte[i] = elements[i].get
+        StringBuilder sb = new StringBuilder(oriIPv6);
+        int index = oriIPv6.indexOf("::");
+        if(index!=-1) {
+            int octets = (oriIPv6.split(":").length)-1;
+            String insert="";
+            for(int i=octets;i<8; i++) {
+                insert+="0000";
+                if(i<=6)
+                    insert+=":";
+		System.out.println("insert == "+insert);
+            }
+            sb.insert(index+1, insert);
         }
+	
+        String[] elements = sb.toString().split(":");
+        String output ="";
+        for(int i=0;i<elements.length; i++) {
+            while(elements[i].length()!=4)
+                elements[i] = "0"+elements[i];
+	    if(i==0)
+		output = elements[i];
+	    else
+               output = output+":"+elements[i];
+        }
+    }
+    
+    private static String macCompress(String oriMAC) {
+        String output="";
+        String[] elements = null;
+        if(oriMAC.contains(".")) {  // IOS
+            elements = oriMAC.split("\\.");
+        } else if(oriMAC.contains(":")) {  //linux
+             elements = oriMAC.split(":");
+        }
+        if(elements!=null) {
+            for (int i=0;i<elements.length;i++)
+                output+=elements[i];
+        }
+
+	if (output.length()!=12) {
+	    System.out.println("something hhas gone wrong with the parse on "+oriMAC+" ("+output+"), returning null");
+            output=null;
+	}
+        return output;
     }
 }
